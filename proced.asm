72ddef5ef0a47ed94d4875efab4d3a7cac452609
Elder <elderzxvm99@gmail.com>
2018-08-17 14:38:02 -0300

    procedure

---
+++ proced.asm
@@ -0,0 +1,44 @@
+section .text
+	global _start
+
+_start:
+	CALL display
+	MOV eax,1
+	int 0x80	
+
+display:
+	MOV ecx,256
+
+next:
+	PUSH ecx
+	MOV eax,4
+	MOV ebx,1
+	MOV ecx,achar
+	MOV edx,1
+	int 80h
+	
+	MOV eax,4
+	MOV ebx,1
+	MOV ecx,space
+	MOV edx,1
+	int 0x80	
+	
+	POP ecx
+	MOV dx,[achar]
+	CMP byte[achar],0dh
+	INC byte[achar]
+	LOOP next
+	RET
+
+newline:
+	MOV dl,10
+	MOV ah,02h
+	int 0x80
+	MOV dl,13
+	int 0x80
+	RET
+
+
+section .data
+	achar DB '0'
+	space DB ' '
